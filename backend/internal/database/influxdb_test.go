9b32bfc8d3a01ec6321cbe7bf9b8a37387a1c914
xyberii4 <61101529+xyberii4@users.noreply.github.com>
2025-02-21 18:22:21 +0000

    Influxdb write test

--- backend/internal/database/influxdb_test.go
+++ backend/internal/database/influxdb_test.go
@@ -1,29 +1,22 @@
 package database
 
 import (
-	"os"
 	"testing"
 
-	"github.com/joho/godotenv"
-	"github.com/nittyquitty/internal/config"
+	"github.com/nittyquitty/internal/models"
 )
 
-func TestNewInfluxDBClient(t *testing.T) {
-	if err := godotenv.Load("../../.env"); err != nil {
-		t.Errorf("Error loading .env file: %v", err)
+// Writes test data to InfluxDB
+func TestInfluxDBWrite(t *testing.T) {
+	testData := models.NicotineConsumption{
+		UserID:   "123",
+		Product:  "test",
+		Mg:       10,
+		Quantity: 1,
+		Cost:     1,
 	}
-	cfg := config.InfluxDBConfig{
-		Url:   os.Getenv("INFLUX_URL"),
-		Token: os.Getenv("INFLUX_TOKEN"),
-		Port:  os.Getenv("INFLUX_PORT"),
-	}
-	c, err := NewInfluxDBClient(cfg)
 
-	defer func() {
-		c.Close()
-	}()
-
-	if err != nil {
-		t.Errorf("Failed to create InfluxDB client: %v", err)
+	if err := influxdbClient.WriteData(testData); err != nil {
+		t.Errorf("Failed to write data to InfluxDB: %v", err)
 	}
 }
