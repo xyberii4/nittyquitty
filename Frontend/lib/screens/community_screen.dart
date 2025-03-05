ea1436822a37c464606c7cdd8d35162dcfcf6b8b
Sneebog <Sethbaldrey@gmail.com>
2025-03-05 14:45:34 +0000

    Created seperate widget files for each screen
    Home screen state used for all and each widget changes according to bottom naviagation

---
+++ Frontend/lib/screens/community_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class PeopleScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('People Page', style: TextStyle(fontSize: 24, color: Colors.white));
+  }
+}
---
+++ Frontend/lib/screens/input_nic_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class InputNicScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Input Nic Page', style: TextStyle(fontSize: 24, color: Colors.white));
+  }
+}
