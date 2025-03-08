bbee13a2fd75a242566c88f018614fc8426b8643
xyberii4 <61101529+xyberii4@users.noreply.github.com>
2025-03-08 15:06:07 +0000

    Added API endpoint for retrieving consumption data

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -13,5 +13,6 @@ func Setup(ic *database.InfluxdbClient, mc *database.MySQLClient) chi.Router {
 
 	r.Get("/api/health", handler.HealthCheck)
 	r.Post("/api/logUsage", handler.LogNicUsage)
+	r.Get("api/getConsumption", handler.GetUserConsumption)
 	return r
 }
