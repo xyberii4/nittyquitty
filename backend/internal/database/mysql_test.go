f1e2b585087e268d0a6699a0b6b34ecd13c9e5fe
xyberii4 <61101529+xyberii4@users.noreply.github.com>
2025-03-25 15:52:39 +0000

    Added test function for deleting user

--- backend/internal/database/mysql_test.go
+++ backend/internal/database/mysql_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestAddUser(t *testing.T) {
 	testUser := models.UserData{
-		Username:     "h",
+		Username:     "mysql_test",
 		Password:     "abcd",
 		Goal:         1,
 		GoalDeadline: "2024-10-31",
@@ -16,5 +16,25 @@ func TestAddUser(t *testing.T) {
 
 	if err := mysqlClient.AddUser(testUser); err != nil {
 		t.Errorf("Failed to add user to MySQL: %v", err)
+	} else {
+		t.Logf("Added new user %s", testUser.Username)
+	}
+}
+
+func TestDeleteUser(t *testing.T) {
+	query := "Select UserID from Users where Username = ?"
+	testUser := models.UserData{
+		Username: "mysql_test",
+		Password: "abcd",
+	}
+
+	if err := mysqlClient.client.QueryRow(query, "mysql_test").Scan(&testUser.UserID); err != nil {
+		t.Errorf("Failed to find user in MySQL: %v", err)
+	}
+
+	if err := mysqlClient.DeleteUser(testUser); err != nil {
+		t.Errorf("Failed to delete user from MySQL: %v", err)
+	} else {
+		t.Logf("Deleted user %s", testUser.Username)
 	}
 }
