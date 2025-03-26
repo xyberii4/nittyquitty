397afa743b3ab2439cfbcd4cbe854e72c97fde31
xyberii4 <61101529+xyberii4@users.noreply.github.com>
2025-03-26 22:08:44 +0000

    .

--- backend/internal/database/mysql.go
+++ backend/internal/database/mysql.go
@@ -36,7 +36,7 @@ func createTable(db *sql.DB) error {
         CigWeeklyUsage INT NOT NULL,
         Goal INT NOT NULL,
         GoalDeadline DATE NOT NULL,
-	WeeklySpending DECIMAL(5,2) NOT NULL,
+        WeeklySpending DECIMAL(5,2) NOT NULL,
         PRIMARY KEY (UserID)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
     `
