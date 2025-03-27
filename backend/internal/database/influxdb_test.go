310d005470850ccd5177de1274c13c0bd2088de5
xyberii4 <61101529+xyberii4@users.noreply.github.com>
2025-03-27 13:28:44 +0000

    Added test function to add one year of fake data to influxdb

--- backend/internal/database/influxdb_test.go
+++ backend/internal/database/influxdb_test.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/nittyquitty/internal/models"
 )
@@ -21,6 +23,31 @@ func TestInfluxDBWrite(t *testing.T) {
 		t.Errorf("Failed to write data to InfluxDB: %v", err)
 	} else {
 		t.Logf("Successfully wrote data to InfluxDB")
+	}
+}
+
+func TestFillInflux(t *testing.T) {
+	now := time.Now()
+	currentDate := now.AddDate(-1, 0, 0)
+
+	for currentDate.Before(now) {
+		weeksPassed := float64(now.Sub(currentDate).Hours() / 16)
+		quantity := int(rand.Float64() * weeksPassed)
+
+		row := models.NicotineConsumption{
+			UserID:    -1,
+			Product:   "cigarettes",
+			Mg:        10.0,
+			Quantity:  quantity,
+			Cost:      0.82 * float64(quantity),
+			Timestamp: currentDate.Format(time.RFC3339),
+		}
+		if err := influxdbClient.WriteData(row); err != nil {
+			t.Errorf("Failed to write data to InfluxDB: %v", err)
+		} else {
+			t.Logf("Successfully wrote data to InfluxDB")
+		}
+		currentDate = currentDate.AddDate(0, 0, 1)
 	}
 }
 
